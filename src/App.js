146776439767eeaba05abc378c1d7bedf8d44e2f
nickgray06 <nickgray87@gmail.com>
2021-12-13 17:57:55 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import  { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <h1>Nicks App</h1>
+      <h2>We have Auth!</h2>
+      <AmplifySignOut />  
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
